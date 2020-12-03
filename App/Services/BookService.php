b745f7d44f518c5dfc0f108983d133f85fd82f3f
momchilmitev <momchilmitev87@gmail.com>
2020-12-03 08:33:41 +0100

    added book service interface and calss implementation

---
+++ App/Services/BookService.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Data\BookDTO;
+use App\Repositories\BookRepositoryInterface;
+
+class BookService implements BookServiceInterface
+{
+    private BookRepositoryInterface $bookRepository;
+
+    private UserServiceInterface  $userService;
+
+    /**
+     * BookService constructor.
+     * @param BookRepositoryInterface $bookRepository
+     * @param UserServiceInterface $userService
+     */
+    public function __construct(BookRepositoryInterface $bookRepository, UserServiceInterface $userService)
+    {
+        $this->bookRepository = $bookRepository;
+        $this->userService = $userService;
+    }
+
+    public function add(BookDTO $bookDto): bool
+    {
+        return $this->bookRepository->insert($bookDto);
+    }
+
+    public function edit(BookDTO $bookDto): bool
+    {
+        return $this->bookRepository->update($bookDto, $bookDto->getId());
+    }
+
+    public function delete(int $id): bool
+    {
+        $this->bookRepository->remove($id);
+    }
+
+    public function getAll(): \Generator
+    {
+        $this->bookRepository->findAll();
+    }
+
+    public function getOneById(int $id): BookDTO
+    {
+
+        return $this->bookRepository->findOneById($id);
+    }
+
+    public function getOneByAuthor(): BookDTO
+    {
+        $currentUser = $this->userService->currentUser();
+        return $this->bookRepository->findAllByAuthorId($currentUser->getId())->current();
+    }
+}
---
+++ App/Services/BookServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Data\BookDTO;
+
+interface BookServiceInterface
+{
+    public function add(BookDTO $bookDto): bool;
+
+    public function edit(BookDTO $bookDto): bool;
+
+    public function delete(int $id): bool;
+
+    /**
+     * @return \Generator|BookDTO[]
+     */
+    public function getAll(): \Generator;
+
+    public function getOneById(int $id): BookDTO;
+
+    public function getOneByAuthor(): BookDTO;
+}
